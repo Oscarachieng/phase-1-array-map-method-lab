ced58ff44ca9185baf6165abb33a1edc40643f30
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-20 19:44:33 +0300

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+
+console.log(tutorials[1].split(' '))
+
+/*
+Pseodocode.
+1. Declare a variable and assign it the fisrt operation of mapping each string item into 
+  an array of strings.
+2. Map each element in the new array into a string of charAt(0) to upperCase().
+3. Join the new stirng to restore the original sentences.
+*/
+const titleCased = (tutorialDocuments)=>{
+  //let joined;
+  const splitted=tutorialDocuments.map(item => item.split(' '));
+  const firstLetterCased=splitted.map(ele => ele.map(ele=>ele.charAt(0).toUpperCase() + ele.slice(1)
+    ))
+   return firstLetterCased
+
+  };
+  
+  //return element
+
+
+
+console.log(titleCased(tutorials))
